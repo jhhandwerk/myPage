dbe9d304a9a0e9c05dcd49349a63e77adf989705
TecchyToni <68844104+jhhandwerk@users.noreply.github.com>
2022-05-29 17:37:08 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,12 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(1400, 480);
 }
 
 function draw() {
-  background(220);
+  background(0);
+  fill(255);
+  noStroke();
+
+  wave = sin(frameCount);
+  ellipse(width/2 + wave * 300,height/2,100,100);
 }
