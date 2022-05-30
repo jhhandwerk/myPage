979f8b0d8c4e7e49011c6059d91a6271b8cf8fcc
Jonathan Handwerk <jonathanhandwerk@Jonathans-MacBook-Air.local>
2022-05-30 22:23:02 +0900

    another test

--- sketch.js
+++ sketch.js
@@ -9,8 +9,5 @@ function draw() {
 
   wave = sin(radians(frameCount));
   ellipse(width/2 + wave * 300,height/2,100,100);
-<<<<<<< HEAD
 }
-=======
 }
->>>>>>> b7aed97521d789b043c427058f89aebb73db402a
