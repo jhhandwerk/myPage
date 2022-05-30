0e4671b5d6b1fc166302a72fc9365ec7650f22ce
Jonathan Handwerk <jonathanhandwerk@Jonathans-MacBook-Air.local>
2022-05-30 22:52:02 +0900

    made a clock sketch

---
+++ sketch2.js
@@ -0,0 +1,16 @@
+function setup() {
+    createCanvas(400, 400);
+}
+
+function draw() {
+    background(0);
+
+    let hr = hour();
+    let mn = minute();
+    let sc = second();
+
+    fill(255);
+    noStroke();
+    text(hr + ':' + mn + ":" + sc, 10, 200);
+}
+
