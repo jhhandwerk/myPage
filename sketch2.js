921525cd07f9779576d92874595a7e96f28e2fd0
Jonathan Handwerk <jonathanhandwerk@Jonathans-MacBook-Air.local>
2022-05-31 22:32:41 +0900

    changed the clock from digital to rotation

--- sketch2.js
+++ sketch2.js
@@ -9,8 +9,19 @@ function draw() {
     let mn = minute();
     let sc = second();
 
-    fill(255);
-    noStroke();
-    text(hr + ':' + mn + ":" + sc, 10, 200);
+    // strokeWeight(8);
+    // stroke(255);
+    // noFill();
+    // ellipse(200, 200, 300, 300);
+
+    strokeWeight(4);
+    stroke(255, 100, 150);
+    let end = map(sc, 0, 60, 0, 360);
+    arc(200, 200, 300, 300, 0, end);
+
+
+    // fill(255);
+    // noStroke();
+    // text(hr + ':' + mn + ":" + sc, 10, 200);
 }
 
