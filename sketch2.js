c2ab66c6dd9e3446177e1b50b1b028e391672339
Jonathan Handwerk <jonathanhandwerk@Jonathans-MacBook-Air.local>
2022-06-09 21:51:19 +0900

    updated clock

--- sketch2.js
+++ sketch2.js
@@ -9,19 +9,20 @@ function draw() {
     let mn = minute();
     let sc = second();
 
-    // strokeWeight(8);
-    // stroke(255);
-    // noFill();
-    // ellipse(200, 200, 300, 300);
 
     strokeWeight(4);
     stroke(255, 100, 150);
-    let end = map(sc, 0, 60, 0, 360);
-    arc(200, 200, 300, 300, 0, end);
+    noFill();
+    let end1 = map(sc, 0, 60, 0, 360);
+    arc(200, 200, 300, 300, 0, end1);
 
+    stroke(150, 100, 255)
+    let end2 = map(mn, 0, 60, 0, 360);
+    arc(200, 200, 280, 280, 0, end2);
 
-    // fill(255);
-    // noStroke();
-    // text(hr + ':' + mn + ":" + sc, 10, 200);
+    stroke(150, 255, 100)
+    let end3 = map(hr % 12, 0, 12, 0, 360);
+    arc(200, 200, 260, 260, 0, end3);
+
 }
 
